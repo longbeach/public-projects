3268ea6c6c3cc494ed2c065c8dbbf351aa51c74b
celinio <cel975@yahoo.com>
2022-02-21 11:01:02 +0100

    Modif

--- kr.ejsoft.spring.with.vuejs/src/front/src/main.js
+++ kr.ejsoft.spring.with.vuejs/src/front/src/main.js
@@ -12,3 +12,5 @@ new Vue({
 }).$mount('#app')
 
 console.log("NODE_ENV vaut :" + process.env.NODE_ENV);
+/* passer en debug pour voir le contenu de process.env */
+console.log("process.env vaut :" + process.env);
